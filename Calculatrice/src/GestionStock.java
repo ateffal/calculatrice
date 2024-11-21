2c40589699c303231eabacadeb29d587eaa3645a
Amine TEFFAL <a.teffal@gmail.com>
2024-11-21 07:30:50 +0100

    Exercice Gestion Stock

--- Calculatrice/src/GestionStock.java
+++ Calculatrice/src/GestionStock.java
@@ -13,7 +13,7 @@ public class GestionStock {
 
     }
 
-    static  void modifierProduit(int code, String nouveauNom, int nouvelleQuantite, double nouveauPrix) {
+    static void modifierProduit(int code, String nouveauNom, int nouvelleQuantite, double nouveauPrix) {
 
     }
 
@@ -32,55 +32,45 @@ public class GestionStock {
     static double calculerValeurStock() {
         return 0;
     }
+
     public static void main(String[] args) {
 
 
-//        int choix ;
-//
-//        //taking value as command line argument.
-//        Scanner in = new Scanner(System.in);
-//
-//
-//
-//        do {
-//
-//            System.out.println(
-//                    """
-//                            ----- Gestion de Stock -----\s
-//                            1. Ajouter un produit\s
-//                            2. Modifier un produit\s
-//                            3. Supprimer un produit\s
-//                            4. Afficher la liste des produits\s
-//                            5. Rechercher un produit\s
-//                            6. Calculer la valeur totale du stock\s
-//                            0. Quitter
-//
-//                            Choisissez une option :
-//                            """
-//            );
-//
-//            choix = in.nextInt();
-//            if(choix <0 | choix >6) {
-//                System.out.println("!!!!!! Choisissez une option entre 0 et 6. !!!!!! ");
-//            }
-//        } while(choix <0 | choix >6);
-//
-//        /* tu dois te servir d'une variable pour gerer le nombre de produits présents dans le stock
-//        * Lorsque tu crée un produit, il faut l'incrémenter (+1)
-//        * Lorsque tu supprimer un produit, il faut le décrémenter (-1)
-//        *
-//        * */
-//    }
+        int choix;
 
-        Double d = Double.valueOf(5.2);
-        double d2 = 5.7;
-        System.out.println(d.toString());
-        Integer i2 = Integer.valueOf(100);
-        i2.doubleValue()
+        //taking value as command line argument.
+        Scanner in = new Scanner(System.in);
 
+
+        do {
+
+            System.out.println(
+                    """
+                            ----- Gestion de Stock -----\s
+                            1. Ajouter un produit\s
+                            2. Modifier un produit\s
+                            3. Supprimer un produit\s
+                            4. Afficher la liste des produits\s
+                            5. Rechercher un produit\s
+                            6. Calculer la valeur totale du stock\s
+                            0. Quitter
+                            
+                            Choisissez une option :
+                            """
+            );
+
+            choix = in.nextInt();
+            if (choix < 0 | choix > 6) {
+                System.out.println("!!!!!! Choisissez une option entre 0 et 6. !!!!!! ");
+            }
+        } while (choix < 0 | choix > 6);
+
+        /* tu dois te servir d'une variable pour gerer le nombre de produits présents dans le stock
+         * Lorsque tu crée un produit, il faut l'incrémenter (+1)
+         * Lorsque tu supprimer un produit, il faut le décrémenter (-1)
+         *
+         * */
     }
-
-
 
 
 }
